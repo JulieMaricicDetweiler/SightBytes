2f14008284bdbbb8268292b6b10849d32e39cae9
Julie Maricic-Detweiler <j.maricicdetweil@ufl.edu>
2024-03-23 20:42:38 -0400

    who is senver

--- application/src/firebase/firebaseConfig.js
+++ application/src/firebase/firebaseConfig.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenverId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
 
