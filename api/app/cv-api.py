a75dca38c53be283aecdb1d67909de6b61c1111c
Taise M <56640569+tmiyazumi@users.noreply.github.com>
2024-03-24 16:07:56 -0400

    test

--- api/app/cv-api.py
+++ api/app/cv-api.py
@@ -29,56 +29,57 @@ class DistanceRequest(BaseModel):
 
 @app.post("/distance")
 async def calculate_distance(request: DistanceRequest):
-    detector = FaceMeshDetector(maxFaces=2)
-    
-    print(request.image_base64)
-    temp = request.image_base64.split(",")[-1]
-    image_data = base64.b64decode(temp)
-    print("Converting...")
-    
-    nparr = np.frombuffer(image_data, np.uint8)
-    print("Past convert 1...")
-    print(nparr)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)    
-    print("Past convert 2...")
-    # pil_image = Image.open(bytes_io)
-     # Check if the image is loaded correctly
-    if img is None:
-        raise HTTPException(status_code=400, detail="Failed to load image")
-
-    # Initialize FaceMeshDetector
-    detector = FaceMeshDetector(maxFaces=2)
-    print("Past convert 3...")
-
-    # Find face mesh in the image
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
-    print("Past convert 4...")
-    img, faces = detector.findFaceMesh(img_rgb)
-    print("Past convert 5...")
-
-    if faces:
-        print(10000)
-        face = faces[0]
-        pointLeft = face[145]
-        pointRight = face[374]
-        w, _ = detector.findDistance(pointLeft, pointRight)
+    try:
+        detector = FaceMeshDetector(maxFaces=2)
         
-        # Constants for calculations
-        W = 6.3  # Average distance between eyes
-        f = 930  # Focal length (adjust this according to your setup)
+        print(request.image_base64)
+        temp = request.image_base64.split(",")[-1]
+        image_data = base64.b64decode(temp)
+        print("Converting...")
         
-        # Calculate distance
-        d = (W * f) / w
+        nparr = np.frombuffer(image_data, np.uint8)
+        print("Past convert 1...")
+        print(nparr)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)    
+        print("Past convert 2...")
+        # pil_image = Image.open(bytes_io)
+        # Check if the image is loaded correctly
+        if img is None:
+            raise HTTPException(status_code=400, detail="Failed to load image")
+
+        # Initialize FaceMeshDetector
+        detector = FaceMeshDetector(maxFaces=2)
+        print("Past convert 3...")
+
+        # Find face mesh in the image
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
+        print("Past convert 4...")
+        img, faces = detector.findFaceMesh(img_rgb)
+        print("Past convert 5...")
+
+        if faces:
+            print(10000)
+            face = faces[0]
+            pointLeft = face[145]
+            pointRight = face[374]
+            w, _ = detector.findDistance(pointLeft, pointRight)
+            
+            # Constants for calculations
+            W = 6.3  # Average distance between eyes
+            f = 930  # Focal length (adjust this according to your setup)
+            
+            # Calculate distance
+            d = (W * f) / w
+            
+            # Display the distance on the image
+            # cv2.putText(img, f'Depth: {d:.2f} cm', (face[10][0] - 75, face[10][1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            print(d)
+            return {"distance": d}
         
-        # Display the distance on the image
-        # cv2.putText(img, f'Depth: {d:.2f} cm', (face[10][0] - 75, face[10][1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        print(d)
-        return {"distance": d}
-    
-    else:
-        return HTTPException(status_code=400, detail="No face detected in the image.")
-    # except Exception as e:
-    #     return HTTPException(status_code=500, detail=str(e))
+        else:
+            return HTTPException(status_code=400, detail="No face detected in the image.")
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
     
 @app.get("/randnum")
 def random_number():
