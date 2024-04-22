5150fc3e02020dddf4d1c983d84b37b9c5dc2600
Taise M <56640569+tmiyazumi@users.noreply.github.com>
2024-04-22 14:55:45 -0400

    python backend change

--- api/app/cv-api.py
+++ api/app/cv-api.py
@@ -43,8 +43,77 @@ class SessionData(BaseModel):
 class DistanceRequest(BaseModel):
     image_base64: str
 
+
+focalPoint = None
+
+@app.post("/calibrate")
+async def calibrate(request: DistanceRequest):
+    global focalPoint
+    try:
+        detector = FaceMeshDetector(maxFaces=2)
+        
+        # print(request.image_base64)
+        temp = request.image_base64.split(",")[-1]
+        image_data = base64.b64decode(temp)
+        print("Converting...")
+        
+        nparr = np.frombuffer(image_data, np.uint8)
+        # print("Past convert 1...")
+        print(nparr)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)    
+        # print("Past convert 2...")
+        # Check if the image is loaded correctly
+        if img is None:
+            raise HTTPException(status_code=400, detail="Failed to load image")
+
+        # Initialize FaceMeshDetector
+        detector = FaceMeshDetector(maxFaces=2)
+        # print("Past convert 3...")
+
+        # Find face mesh in the image
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
+        # print("Past convert 4...")
+        img, faces = detector.findFaceMesh(img_rgb)
+        # print("Past convert 5...")
+
+        if faces:
+            print(10000)
+            face = faces[0]
+            pointLeft = face[145]
+            pointRight = face[374]
+            w, _ = detector.findDistance(pointLeft, pointRight)
+            
+            # find focal point first 
+            W = 6.3 # Average distance between eyes
+
+            # finding focal point
+            d = 30
+            f = (w*d)/ W
+            # Constants for calculations
+          
+            # f = 650  # Focal length (adjust this according to your setup)
+            
+            # Calculate distance
+            # d = (W * f) / w
+            
+            # Display the distance on the image
+            # cv2.putText(img, f'Depth: {d:.2f} cm', (face[10][0] - 75, face[10][1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            print(f)
+            focalPoint = f
+            return {"focalPoint": f}
+
+        else:
+            return HTTPException(status_code=400, detail="No face detected in the image.")
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+
+
+
 @app.post("/distance")
 async def calculate_distance(request: DistanceRequest):
+    global focalPoint
+    print("FOCALLLLL")
+    print(focalPoint)
     try:
         detector = FaceMeshDetector(maxFaces=2)
         
@@ -87,7 +156,7 @@ async def calculate_distance(request: DistanceRequest):
             # f = (w*d)/ W
             # Constants for calculations
           
-            f = 650  # Focal length (adjust this according to your setup)
+            f = focalPoint  # Focal length (adjust this according to your setup)
             
             # Calculate distance
             d = (W * f) / w
@@ -129,16 +198,47 @@ async def score_session(data: SessionData):
     totalRightQuestions, rightCorrectAnswers, rightDetails = calculate_results(data.rightEyeQuestions)
     curr_date = datetime.now().date()
 
+    def get_test_result(x):
+        if (x == 1):
+            return "20/200";
+        elif (x == 2):
+            return "20/100";
+        elif (x == 3):
+            return "20/70";
+        elif (x == 4):
+            return "20/50";
+        elif (x == 5):
+            return "20/40";
+        elif (x == 6):
+            return "20/30";
+        elif (x == 7):
+            return "20/25";
+        elif (x == 8):
+            return "20/20";
+        elif (x == 9):
+            return "20/15";
+        elif (x == 10):
+            return "20/13";
+        else:
+            return "20/200";
+    
+    leftResult = get_test_result(leftCorrectAnswers);
+    rightResult = get_test_result(rightCorrectAnswers);
+
+    
+
     return {
         "leftEye": {
             "totalQuestions": totalLeftQuestions,
             "correctAnswers": leftCorrectAnswers,
-            "questions": leftDetails
+            "questions": leftDetails,
+            "result": leftResult
         },
         "rightEye": {
             "totalQuestions": totalRightQuestions,
             "correctAnswers": rightCorrectAnswers,
-            "questions": rightDetails
+            "questions": rightDetails,
+            "result": rightResult
         },
         "date": curr_date
     }
